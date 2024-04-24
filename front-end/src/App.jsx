12b497b92604106f942276921aa03c03c1cae8f1
Sadab Azhar <sadabazhar84@gmail.com>
2024-04-24 15:37:56 +0530

    Implement React Router in App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//Components
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
+import Carousel from './Components/Carousel'
+import Contact from './Components/Contact'
+import Courses from './Components/Courses'
+import Footer from './Components/Footer'
+import Services from './Components/Services'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Navbar/>  
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About />} />
+          <Route path='/carousel' element={<Carousel/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/courses' element={<Courses/>} />
+          <Route path='/footer' element={<Footer/>} />
+          <Route path='/Services' element={<Services/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
