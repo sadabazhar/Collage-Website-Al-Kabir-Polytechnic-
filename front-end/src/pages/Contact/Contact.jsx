583f64b5090831ef9fde4d0f2228d4de14bca705
Sadab Azhar <sadabazhar84@gmail.com>
2024-07-02 23:31:14 +0530

    Exchange Contact photo

--- front-end/src/pages/Contact/Contact.jsx
+++ front-end/src/pages/Contact/Contact.jsx
@@ -4,9 +4,6 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import LondonLocationImg from '../../utils/images/London-location.jpg';
-import ManchesterLocationImg from '../../utils/images/Manchester-location.jpg';
-import LiverpoolLocationImg from '../../utils/images/Liverpool-location.jpg';
 
 function Contact() {
   return (
@@ -14,7 +11,7 @@ function Contact() {
         <header className='height-75'>
             <div className='container h-100 d-flex flex-column align-items-center justify-content-center text-light'>
                 <h1 className='text-center fw-semibold'>Get In Touch</h1>
-                <p className='text-center w-75 mb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae pariatur qui quos aspernatur, voluptatem autem possimus esse quo consequatur omnis, soluta consectetur ullam ipsum cum!</p>
+
             </div>
         </header>
 
@@ -77,7 +74,7 @@ function Contact() {
                         }
                         `}
                     </style>
-                <Button className='custom-hover' variant="danger btn-lg" type='submit'>Submit</Button>
+                <Button className='custom-hover' variant="danger btn-lg" type='submit'>Sent Your Message</Button>
             </Form>
         </div>
 
